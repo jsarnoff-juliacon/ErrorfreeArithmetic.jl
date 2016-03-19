6aa975d4dfc9fb24c2d47691c577a45baf082cd9
Jeffrey Sarnoff <Jeffrey-Sarnoff@users.noreply.github.com>
2016-03-18 23:21:40 -0400

    Update single.jl

--- src/single.jl
+++ src/single.jl
@@ -1,1 +1,14 @@
 
+function eftSqr(a::AbstractFloat)
+    x = a * a
+    y = fma(a, a, -x)
+    x,y
+end
+
+function eftCub(a::AbstractFloat)
+    p = a*a; e = fma(a, a, -p)
+    x = p*a; p = fma(p, a, -x)
+    y = e*a
+    x,y
+end
+
