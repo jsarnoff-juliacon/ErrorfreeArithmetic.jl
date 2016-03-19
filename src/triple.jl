8da2db6acfdb0ce604deb09523fe7196fc440c2d
Jeffrey Sarnoff <Jeffrey-Sarnoff@users.noreply.github.com>
2016-03-19 01:02:50 -0400

    Update triple.jl

--- src/triple.jl
+++ src/triple.jl
@@ -1,4 +1,12 @@
 
+function eftAdd3{T<:Float64}(a::T,b::T,c::T)
+    s,t = eftAdd(b, c)
+    x,u = eftAdd(a, s)
+    y,z = eftAdd(u, t)
+    x,y = eftAddGTE(x, y)
+    x,y,z
+end
+
 function eftMul3to4{T<:AbstractFloat}(a::T, b::T, c::T)
     p,e = eftMul(a,b)
     x,y = eftMul(p,c)
