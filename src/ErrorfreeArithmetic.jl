da3589e7ce9bce2c560df9f701cfa6dc7b4ebd3f
Jeffrey Sarnoff <Jeffrey-Sarnoff@users.noreply.github.com>
2016-03-19 00:59:52 -0400

    Update ErrorfreeArithmetic.jl

--- src/ErrorfreeArithmetic.jl
+++ src/ErrorfreeArithmetic.jl
@@ -4,7 +4,8 @@ import Base: (+),(-),(*),fma
 
 export eftIncr, eftDecr, eftSqr,          # single
        eftAdd, eftSub, eftMul,            # double
-       eftAdd3, eftMul3, eftFMA           # triple
+       eftAdd3, eftMul3, eftFMA,          # triple
+       eftMul3to3, eftMult3to2
 
 include "single.jl"
 include "double.jl"
