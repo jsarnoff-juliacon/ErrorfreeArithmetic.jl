b629c8eeee896d8c08cccfe70d50078157354ac4
Jeffrey Sarnoff <Jeffrey-Sarnoff@users.noreply.github.com>
2016-03-19 01:31:47 -0400

    Update double.jl

--- src/double.jl
+++ src/double.jl
@@ -1,1 +1,33 @@
 
+function eftAdd{T<:AbstractFloat}(a::T, b::T)
+  x = a + b
+  t = x - a
+  y = (a - (x - t)) + (b - t)
+  x,y
+end
+
+function eftAddGTE{T<:AbstractFloat}(a::T, b::T)
+  x = a + b
+  y = b - (x - a)
+  x,y
+end
+
+function eftSub{T<:AbstractFloat}(a::T, b::T)
+  x = a - b
+  t = x - a
+  y = (a - (x - t)) - (b + t)
+  x,y
+end
+
+function eftSubGTE{T<:AbstractFloat}(a::T, b::T)
+  x = a - b
+  y = (a - x) - b
+  x,y
+end
+
+function eftMul{T<:AbstractFloat}(a::T, b::T)
+    x = a * b
+    y = fma(a, b, -x)
+    x,y
+end
+
