67e313b589860e308f07bd439be20fb1641bb6e9
Jeffrey Sarnoff <Jeffrey-Sarnoff@users.noreply.github.com>
2016-03-22 11:57:32 -0400

    Update double.jl

--- src/double.jl
+++ src/double.jl
@@ -1,31 +1,31 @@
 
-function eftAdd{T<:AbstractFloat}(a::T, b::T)
+function eftAdd{T<:StdFloat}(a::T, b::T)
   x = a + b
   t = x - a
   y = (a - (x - t)) + (b - t)
   x,y
 end
 
-function eftAddGTE{T<:AbstractFloat}(a::T, b::T)
+function eftAddGTE{T<:StdFloat}(a::T, b::T)
   x = a + b
   y = b - (x - a)
   x,y
 end
 
-function eftSub{T<:AbstractFloat}(a::T, b::T)
+function eftSub{T<:StdFloat}(a::T, b::T)
   x = a - b
   t = x - a
   y = (a - (x - t)) - (b + t)
   x,y
 end
 
-function eftSubGTE{T<:AbstractFloat}(a::T, b::T)
+function eftSubGTE{T<:StdFloat}(a::T, b::T)
   x = a - b
   y = (a - x) - b
   x,y
 end
 
-function eftMul{T<:AbstractFloat}(a::T, b::T)
+function eftMul{T<:StdFloat}(a::T, b::T)
     x = a * b
     y = fma(a, b, -x)
     x,y
