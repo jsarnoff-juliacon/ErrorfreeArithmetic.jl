6dd16b77b3b214caa07c6154a0a89467e37419df
Jeffrey Sarnoff <Jeffrey-Sarnoff@users.noreply.github.com>
2016-03-25 18:41:39 -0400

    Update single.jl

--- src/single.jl
+++ src/single.jl
@@ -1,35 +1,33 @@
 
 function eftSqr{T<:StdFloat}(a::T)
-    x = a * a
-    y = fma(a, a, -x)
-    x,y
+    hi = a * a
+    lo = fma(a, a, -hi)
+    hi,lo
 end
 
 function eftIncr{T<:StdFloat}(a::T)
     b = one(T)
-    x = a + b
-    t = x - a
-    y = (a - (x - t)) + (b - t)
-    x,y
+    hi = a + b
+    t = hi - a
+    lo = (a - (hi - t)) + (b - t)
+    hi,lo
 end
 
 function eftDecr{T<:StdFloat}(a::T)
     b = one(T)
-    x = a - b
-    t = x - a
-    y = (a - (x - t)) - (b + t)
-    x,y
+    hi = a - b
+    t = hi - a
+    lo = (a - (hi - t)) - (b + t)
+    hi,lo
 end
 
 function eftRecip{T<:StdFloat}(b::T)
-    a = one(T)
-    q0 = one(T)/b
-    r = fma(-b,q0,a)
-    q1 = r/b
-    r = fma(-b,q1,r)
-    q2 = r/b
-    q1 += q2
-    q0,q1
+    hi = one(T)/b
+    r = fma(-b,hi,one(T))
+    lo = r/b
+    r = fma(-b,lo,r)
+    lo += r/b
+    hi,lo
 end
 
 #=
