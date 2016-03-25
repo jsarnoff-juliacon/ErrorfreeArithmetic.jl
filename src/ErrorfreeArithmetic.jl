14ed087203a9e5b1ca2b546f613b361bef413705
Jeffrey Sarnoff <Jeffrey-Sarnoff@users.noreply.github.com>
2016-03-25 19:28:58 -0400

    Update ErrorfreeArithmetic.jl

--- src/ErrorfreeArithmetic.jl
+++ src/ErrorfreeArithmetic.jl
@@ -4,8 +4,8 @@ import Base: (+),(-),(*),fma
 
 import JAS: StdFloat
 
-export eftIncr, eftDecr, eftSqr,          # single
-       eftAdd, eftSub, eftMul,            # double
+export eftIncr, eftDecr, eftSqr, eftRecip # single
+       eftAdd, eftSub, eftMul, eftDiv,    # double
        eftAddGTE, eftSubGTE,
        eftAdd3, eftMul3,                  # triple
        eftAdd3to2, eftMul3to4, eftMult3to2,
