089555a66d6d0468c742c1b09d6d28a89b1aae01
Jeffrey Sarnoff <Jeffrey-Sarnoff@users.noreply.github.com>
2016-03-26 13:30:06 -0400

    Update quadruple.jl

--- src/quadruple.jl
+++ src/quadruple.jl
@@ -27,3 +27,32 @@ function eftAdd{T<:StdFloat}(a::T,b::T,c::T,d::T)
 end
 
 eftAdd4{T<:StdFloat}(a::T,b::T,c::T,d::T) = eftAdd(a,b,c,d)
+
+
+function eftAddGTE{T<:StdFloat}(a::T,b::T,c::T,d::T)
+
+   w=x=y=z=zero(T)
+   
+   w,d = eftAddGTE(c,d)
+   w,c = eftAddGTE(b,w)
+   a,b = eftAddGTE(a,w)
+   
+   w,x = a,b
+   if x != zero(T)
+       x,y = eftAddGTE(x,c)
+       if y != zero(T)
+           y,z = eftAddGTE(y,d)
+       else
+           x,y = eftAddGTE(x,d)
+       end
+   else
+       w,x = eftAddGTE(w,c)
+       if x != zero(T)
+           x,y = eftAddGTE(x,d)
+       else
+           w,x = eftAddGTE(w,d)
+       end
+   end
+   
+   w,x,y,z
+end
