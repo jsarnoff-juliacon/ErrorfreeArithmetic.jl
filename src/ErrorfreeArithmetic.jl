5afcb2d674a6c6b8b383981818a118bbde852923
Jeffrey Sarnoff <Jeffrey-Sarnoff@users.noreply.github.com>
2016-04-09 02:05:17 -0400

    Update ErrorfreeArithmetic.jl

--- src/ErrorfreeArithmetic.jl
+++ src/ErrorfreeArithmetic.jl
@@ -4,13 +4,13 @@ import Base: (+),(-),(*),(/),fma,sqrt
 
 import Genera: Float, StdFloat
 
-export eftIncr, eftDecr, eftSqr, eftRecip,  # single
+export eftIncr, eftDecr, eftSqr, eftRecip,   # single
        accSqrt, accSqrtForSign,
-       eftAdd, eftSub, eftMul, eftDiv,      # double
+       eftAdd, eftSub, eftMul, eftDiv,       # double
        eftAddGTE, eftSubGTE, eftDivApprox,
-       eftAddAs2, eftMulAs4, eftMulAs2, # triple (also eftAdd, eftAddGTE, eftMul)
+       eftAddAs2, eftMulAs4, eftMulAs2,      # triple (also eftAdd, eftAddGTE, eftMul)
        eftFMA, eftFMA, eftFMAas2, eftFMSas2,
-       eftAddAs3, eftAddGTEas3              # quadruple (also eftAdd, eftAddGTE)
+       eftAddAs3, eftAddGTEas3               # quadruple (also eftAdd, eftAddGTE)
 
 include("single.jl")
 include("double.jl")
