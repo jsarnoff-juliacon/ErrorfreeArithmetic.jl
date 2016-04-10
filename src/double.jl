3a21d50abd559c28fefae52863e841c4b70d070e
Jeffrey Sarnoff <Jeffrey-Sarnoff@users.noreply.github.com>
2016-04-10 10:23:04 -0400

    Update double.jl

--- src/double.jl
+++ src/double.jl
@@ -41,7 +41,7 @@ function eftDiv{T<:StdFloat}(a::T, b::T)
 end
 
 # for use with directed rounding, when only the sign of the lo part is needed
-function eftDivApprox{T<:AbstractFloat}(a::T, b::T)
+function eftDivApprox{T<:StdFloat}(a::T, b::T)
     hi = a/b
     loApprox = -fma(b,hi,-a) # sign is correct, value is actual*b
     hi,loApprox
