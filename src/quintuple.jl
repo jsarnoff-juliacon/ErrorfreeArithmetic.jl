e4f747d6a3f7a6ee10b6b50cd00b32ce14b2046c
Jeffrey Sarnoff <Jeffrey-Sarnoff@users.noreply.github.com>
2016-05-08 06:16:14 -0400

    Update quintuple.jl

--- src/quintuple.jl
+++ src/quintuple.jl
@@ -14,9 +14,36 @@ function renormalizeAs4{T<:StdFloat}(a0::T,a1::T,a2::T,a3::T,a4::T)
    s,t1 = eftSumGTE(t1,s)
    b1,t0 = eftSumGTE(t0,s)
    s,t1 = eftSumGTE(t1,t2)
-   b0,t0 = eftSumGTE(t0,s)
+   b2,t0 = eftSumGTE(t0,s)
    b3 = t0+t1
    
    b0,b1,b2,b3
 end
 
+function renormalizeAs3{T<:StdFloat}(a0::T,a1::T,a2::T,a3::T,a4::T)
+   s,t3 = eftSumGTE(a3,a4)
+   s,t2 = eftSumGTE(a2,s)
+   s,t1 = eftSumGTE(a1,s)
+   b0,t0 = eftSumGTE(a0,s)
+   
+   s,t2 = eftSumGTE(t2,t3)
+   s,t1 = eftSumGTE(t1,s)
+   b1,t0 = eftSumGTE(t0,s)
+   s,t1 = eftSumGTE(t1,t2)
+   b2 = t0+s
+
+   b0,b1,b2
+end
+
+function renormalizeAs2{T<:StdFloat}(a0::T,a1::T,a2::T,a3::T,a4::T)
+s,t3 = eftSumGTE(a3,a4)
+   s,t2 = eftSumGTE(a2,s)
+   s,t1 = eftSumGTE(a1,s)
+   b0,t0 = eftSumGTE(a0,s)
+   
+   s = t2+t3
+   s = t1+s
+   b1 = t0+s
+   
+   b0,b1
+end   
