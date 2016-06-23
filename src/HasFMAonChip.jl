e127fc498f4312c7d1b20c04de63108fa40a8e09
Jeffrey Sarnoff <jsarnoff.juliacon@gmail.com>
2016-06-23 18:37:17 -0400

    Update HasFMAonChip.jl
    improved test

--- src/HasFMAonChip.jl
+++ src/HasFMAonChip.jl
@@ -1,7 +1,4 @@
 fmatime = matime = 1.0;
-for i in 1:500 matime=min(matime, (@timed muladd(3.0,2.0,1.0))[2]) end;
-for i in 1:500 fmatime=min(fmatime, (@timed fma(3.0,2.0,1.0))[2]) end;
-
-const HasFMAonChip = fmatime < matime
-
-
+for i in 1:5 matime=min(matime, (@timed muladd(3.0,2.0,1.0))[2]) end;
+for i in 1:5 fmatime=min(fmatime, (@timed fma(3.0,2.0,1.0))[2]) end;
+const HasFMAonChip = fmatime < 2*matime;
